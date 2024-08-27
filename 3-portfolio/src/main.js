11486d2c8a1a7af1f35c58873e3c3ec2d3dffc94
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-08-27 12:06:42 +0900

    [3-portfolio] feat: add scroll effect to section tags

--- 3-portfolio/src/main.js
+++ 3-portfolio/src/main.js
@@ -1,11 +1,35 @@
-function handleScroll(el) {
+import debounce from './utils/debounce.js'
+
+function handleScrollHeader(el) {
+  if (!el) return
   el.offsetHeight < window.scrollY
     ? el.classList.add('scrolled')
     : el.classList.remove('scrolled')
 }
 
+function handleScrollSection(el, headerEl) {
+  if (!el || !headerEl) return
+  const rect = el.getBoundingClientRect()
+  rect.top < headerEl.scrollHeight && rect.bottom > 0
+    ? (el.style.filter = `blur(${Math.abs(rect.top) / 100}px)`)
+    : (el.style.filter = `none`)
+}
+
+const onScroll = debounce(() => {
+  const headerEl = document.querySelector('header')
+  if (!headerEl) return
+
+  const sections = ['#home', '#about', '#skill', '#work', '#testimonial']
+  const sectionsEl = sections
+    .map(id => document.querySelector(id))
+    .filter(Boolean)
+
+  handleScrollHeader(headerEl)
+  sectionsEl.forEach(el => handleScrollSection(el, headerEl))
+}, 20)
+
 function setEventListeners() {
-  document.onscroll = () => handleScroll(document.querySelector('header'))
+  document.addEventListener('scroll', onScroll)
 }
 
 ;(function init() {
